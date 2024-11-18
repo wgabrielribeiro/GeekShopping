ade89e761147ca524d56f3c7407330e82bdd9a34
Gabriel Ribeiro <ogabrielribeiro97@gmail.com>
2024-11-18 10:58:47 -0300

    Feat: Implementação de API Gatway com Ocelot para controlar e intermediar acessos.

---
+++ GeekShopping.APIGateway/Program.cs
@@ -0,0 +1,27 @@
+using Microsoft.IdentityModel.Tokens;
+using Ocelot.DependencyInjection;
+using Ocelot.Middleware;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+builder.Services.AddAuthentication("Bearer")
+    .AddJwtBearer("Bearer", opt =>
+    {
+        opt.Authority = "https://localhost:4435/";
+        opt.TokenValidationParameters = new TokenValidationParameters
+        {
+            ValidateAudience = false
+        };
+    });
+
+builder.Services.AddOcelot();
+
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+
+app.UseOcelot();
+
+app.Run();
